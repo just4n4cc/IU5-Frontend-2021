c88deff03837c825ddf80ae17bfed4e1b6b99701
Alexandr <kas19u502@student.bmstu.ru>
2021-03-27 00:26:16 +0300

    Added lab3

--- lab3/2.js
+++ lab3/2.js
@@ -7,8 +7,49 @@
  * Примеры:
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
+
 function getMinMax(str) {
-    // code here
+    let res = { min: 0, max: 0 }
+    let zeroFound = true;
+    let temp = '';
+    for (let i = 0; i < str.length; ++i) {
+        if (!isNaN(parseInt(str[i]))) {
+            temp = temp.concat(str[i]);
+            continue;
+        } else if (str[i] === '-') {
+            if (temp.length === 0 && !isNaN(parseInt(str[i + 1]))) {
+                temp = temp.concat(str[i], str[++i]);
+                continue;
+            }
+        } else if (str[i] === '.') {
+            if (temp.length !== 0 && !isNaN(parseInt(str[i + 1])) && !temp.includes('.')) {
+                temp = temp.concat(str[i], str[++i])
+                continue;
+            }
+        }
+
+        if (temp.length !== 0) {
+            if (temp.includes('.')) {
+                temp = parseFloat(temp);
+            } else {
+                temp = parseInt(temp);
+            }
+            if (zeroFound) {
+                zeroFound = false;
+                res.min = res.max = temp;
+            } else {
+                if (res.min > temp) {
+                    res.min = temp;
+                }
+                if (res.max < temp) {
+                    res.max = temp;
+                }
+            }
+            temp = '';
+        }
+    }
+
+    return res;
 }
 
 module.exports = getMinMax;
