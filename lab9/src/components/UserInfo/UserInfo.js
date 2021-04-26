0c60fb7a406d3142441ceb62238a3d72d061c94e
Alexandr <kas19u502@student.bmstu.ru>
2021-04-26 13:38:28 +0300

    Please

--- lab9/src/components/UserInfo/UserInfo.js
+++ lab9/src/components/UserInfo/UserInfo.js
@@ -1,5 +1,5 @@
 import "./UserInfo.css"
-import door from "../../img/door.svg"
+import {ReactComponent as Door} from "../../img/door.svg"
 import {Link, useHistory} from "react-router-dom"
 
 export function UserInfo(props) {
@@ -22,7 +22,8 @@ export function UserInfo(props) {
             </div>
             <div className="rhs">
                 <img src={props.data.avatar_url} alt=""/>
-                <img onClick={(e)=>handleClick(e)} id="door" src={door} alt=""/>
+                <Door onClick={(e)=>handleClick(e)} id="door" alt=""/>
+                {/* <img onClick={(e)=>handleClick(e)} id="door" src={door} alt=""/> */}
             </div>
         </div>
     );
